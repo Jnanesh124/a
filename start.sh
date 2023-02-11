6f6dca63b4c5c98f4ab43cc39bbfb5b94ad1e104
Jnanesh <99884442+Jnanesh124@users.noreply.github.com>
2023-02-11 15:49:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jnanesh124/a  /a
+  git clone https://github.com/Jnanesh124/thunder  /thunder 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /a
+  git clone $UPSTREAM_REPO /thunder 
 fi
-cd /jn
+cd /thunder 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
